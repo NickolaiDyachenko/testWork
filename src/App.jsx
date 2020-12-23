fdccddb3c3b1916bee2eca3197fc7de6206b11b6
NickolaiDyachenko <40198663+NickolaiDyachenko@users.noreply.github.com>
2020-12-23 21:05:26 +0200

    Files added
    FIles added indeed

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { Observable } from "rxjs";
+
+function App() {
+  const [timeState, setTimeState] = useState({ h: 0, m: 0, s: 0 });
+
+  const [subscriberState, setSubscriberState] = useState();
+
+  const [isTimerActiveState, setIsTimerActiveState] = useState(false);
+
+  const [isWaitBtnTriggered, setIsWaitBtnTriggered] = useState(false);
+
+  const onClickResetHandler = (e) => {
+    if (!isTimerActiveState) setIsTimerActiveState(!isTimerActiveState);
+    subscriberState.unsubscribe();
+
+    setTimeState({ h: 0, m: 0, s: 0 });
+
+    setSubscriberState(
+      createObs({ h: 0, m: 0, s: 0 }).subscribe((v) => {
+        setTimeState({ ...v });
+      })
+    );
+  };
+
+  const onClickStartStopHandler = (e) => {
+    if (!isTimerActiveState) {
+      setSubscriberState(
+        createObs(timeState).subscribe((v) => {
+          setTimeState({ ...v });
+        })
+      );
+    } else {
+      subscriberState.unsubscribe();
+      setTimeState({ h: 0, m: 0, s: 0 });
+    }
+    setIsTimerActiveState(!isTimerActiveState);
+  };
+
+  const onClickWaitHandler = (e) => {
+    if (isWaitBtnTriggered) {
+      setIsTimerActiveState(!isTimerActiveState);
+      subscriberState.unsubscribe();
+    }
+
+    setIsWaitBtnTriggered(true);
+
+    setTimeout(() => setIsWaitBtnTriggered(false), 300);
+  };
+
+  return (
+    <div className="timer">
+      <div className="displayTime">
+        <p className="hours">
+          {timeState.h > 9 ? timeState.h : "0" + timeState.h}
+        </p>
+        <p className="minutes">
+          {timeState.m > 9 ? timeState.m : "0" + timeState.m}
+        </p>
+        <p className="seconds">
+          {timeState.s > 9 ? timeState.s : "0" + timeState.s}
+        </p>
+      </div>
+      <button id="startStopButton" onClick={onClickStartStopHandler}>
+        {isTimerActiveState ? "Stop" : "Start"}
+      </button>
+      <button id="waitButton" onClick={onClickWaitHandler}>
+        Wait
+      </button>
+      <button id="resetButton" onClick={onClickResetHandler}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+function createObs(timeState) {
+  return new Observable((subscriber) => {
+    let intervalId = setInterval(() => {
+      if (timeState.s >= 59) {
+        timeState.m++;
+        timeState.s = 0;
+        if (timeState.m >= 59) {
+          timeState.h++;
+          timeState.m = 0;
+        }
+      } else timeState.s++;
+
+      subscriber.next(timeState);
+    }, 1000);
+
+    return function unsubscribe() {
+      clearInterval(intervalId);
+    };
+  });
+}
+
+export default App;
